daa3727342923b51e424d43e6d88f1db2520cf82
Devashish01 <malhotradevashish01@gmail.com>
2018-09-25 12:08:35 +0530

    rotate array

---
+++ cpp/rotate/method1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+void rotate(int arr[],int n)
+{
+int x=arr[n-1];
+for(int i=n-2;i>=0;i--)
+{
+arr[i+1]=arr[i];
+
+}
+arr[0]=x;
+
+}
+void print(int arr[],int n)
+{
+for(int i=0;i<n;i++)
+cout<<arr[i]<<" ";
+}
+int main(int argc, char **argv)
+{
+int arr[100];
+//int arr[]={1,2,3,4,5};
+int n=argc-1;//=sizeof(arr)/sizeof(arr[0]);
+for(int i=0;i<n;i++)
+{
+arr[i]=(atoi)(argv[i+1]);
+
+}
+rotate(arr,n);
+print(arr,n);
+}
